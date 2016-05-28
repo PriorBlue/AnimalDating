1c9ed6aee00fde20df1afa1335b664ce786048cc
Marcus Ihde <marcusplate@googlemail.com>
2016-05-28 12:19:04 +0200

    Initial.

---
+++ love2d/main.lua
@@ -0,0 +1,30 @@
+require('lib/32linesofgoodness')
+require("lib/postshader")
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.mousepressed(x, y, button)
+
+end
+
+function love.mousemoved(x, y, dx, dy)
+
+end
